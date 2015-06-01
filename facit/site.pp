8b860da4f4620970aade4c82fc514a03e74d94be
Jakob Kylberg <jakob.kylberg@gmail.com>
2015-06-01 19:45:12 +0200

    Improved Nexus configuration and added nodes for prod and test.

--- facit/site.pp
+++ facit/site.pp
@@ -1,17 +1,40 @@
-class {'nexus':
-    url      => "http://192.168.33.10:8081/nexus",
-    username => "admin",
-    password => "admin123"
+define backend_app($version) { 
+
+    class {'nexus':
+        url      => "http://192.168.33.10:8081/nexus",
+        username => "admin",
+        password => "admin123"
+    }
+
+    file { "/opt/cicd-lab-backend":
+        ensure => "directory",
+    } ->
+    exec { 'stop-app':
+        command => '/usr/sbin/service cicd-lab-backend.sh stop'
+    } ->
+    exec { 'clean dir':
+        command => '/usr/bin/find /opt/cicd-lab-backend -name "*.jar" -exec rm -f {} \;'
+    } ->
+    nexus::artifact {'cicd-lab-backend':
+        gav        => "se.omegapoint:cicd-lab-backend:${version}",
+        repository => "public",
+        output     => "/opt/cicd-lab-backend/cicd-lab-backend-${version}.jar",
+        ensure     => "update"
+    } ->
+    exec { 'start-app':
+        command => '/usr/sbin/service cicd-lab-backend.sh start'
+    }
 }
 
-exec { 'stop-app':
-  command => '/usr/sbin/service cicd-lab-backend.sh stop',
-} ~>
-nexus::artifact {'cicd-lab-backend':
-   gav        => "se.omegapoint:cicd-lab-backend:1.0",
-   repository => "public",
-   output     => "/opt/cicd-lab-backend/cicd-lab-backend-1.0.jar"
-} ~>
-exec { 'start-app':
-  command => '/usr/sbin/service cicd-lab-backend.sh start'
+node 'test' {
+    backend_app { 'backend_app':
+    version => '1.0.8'
+    }
 }
+
+node 'prod' {
+    backend_app { 'backend_app':
+    version => '1.0.7'
+    }
+}
+
