73f4aca56d0e6747b4e4f57f1f76ed323591ce8e
Jakob Kylberg <jakob.kylberg@gmail.com>
2015-06-04 21:41:45 +0200

    Formatting of the puppet files.

--- facit/puppet/site.pp
+++ facit/puppet/site.pp
@@ -1,4 +1,4 @@
-class {'nexus':
+class { 'nexus':
   url      => "http://192.168.33.10:8081/nexus",
   username => "admin",
   password => "admin123"
@@ -6,29 +6,29 @@ class {'nexus':
 
 define backend_app($version) {
 
-    file { "/opt/cicd-lab-backend":
-        ensure => "directory"
-    } ->
-    exec { 'stop-app':
-        command => '/usr/sbin/service cicd-lab-backend.sh stop'
-    } ->
-    exec { 'clean dir':
-        command => '/usr/bin/find /opt/cicd-lab-backend -name "*.jar" -exec rm -f {} \;'
-    } ->
-    nexus::artifact {'cicd-lab-backend':
-        gav        => "se.omegapoint:cicd-lab-backend:${version}",
-        repository => "public",
-        output     => "/opt/cicd-lab-backend/cicd-lab-backend-${version}.jar",
-        ensure     => "update"
-    } ->
-    exec { 'start-app':
-        command => '/usr/sbin/service cicd-lab-backend.sh start'
-    }
+  file { "/opt/cicd-lab-backend":
+    ensure => "directory"
+  } ->
+  exec { 'stop-app':
+    command => '/usr/sbin/service cicd-lab-backend.sh stop'
+  } ->
+  exec { 'clean dir':
+    command => '/usr/bin/find /opt/cicd-lab-backend -name "*.jar" -exec rm -f {} \;'
+  } ->
+  nexus::artifact { 'cicd-lab-backend':
+    gav        => "se.omegapoint:cicd-lab-backend:${version}",
+    repository => "public",
+    output     => "/opt/cicd-lab-backend/cicd-lab-backend-${version}.jar",
+    ensure     => "update"
+  } ->
+  exec { 'start-app':
+    command => '/usr/sbin/service cicd-lab-backend.sh start'
+  }
 }
 
 define frontend_app($version) {
 
-  nexus::artifact {'cicd-lab-frontend':
+  nexus::artifact { 'cicd-lab-frontend':
     gav        => "se.omegapoint:cicd-lab-frontend:${version}",
     repository => "public",
     packaging  => 'tar.gz',
@@ -42,7 +42,7 @@ define frontend_app($version) {
 }
 
 node 'default' {
-			      
+
   $backend_version = hiera("cicd-lab-backend_version")
   $frontend_version = hiera("cicd-lab-frontend_version")
 
