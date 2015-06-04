39fd671f771055077ef1c7222b613664f7700a87
Jakob Kylberg <jakob.kylberg@gmail.com>
2015-06-04 21:39:19 +0200

    Deploy the frontend app using puppet.

--- facit/puppet/site.pp
+++ facit/puppet/site.pp
@@ -1,13 +1,13 @@
-define backend_app($version) { 
+class {'nexus':
+  url      => "http://192.168.33.10:8081/nexus",
+  username => "admin",
+  password => "admin123"
+}
 
-    class {'nexus':
-        url      => "http://192.168.33.10:8081/nexus",
-        username => "admin",
-        password => "admin123"
-    }
+define backend_app($version) {
 
     file { "/opt/cicd-lab-backend":
-        ensure => "directory",
+        ensure => "directory"
     } ->
     exec { 'stop-app':
         command => '/usr/sbin/service cicd-lab-backend.sh stop'
@@ -26,11 +26,31 @@ define backend_app($version) {
     }
 }
 
+define frontend_app($version) {
+
+  nexus::artifact {'cicd-lab-frontend':
+    gav        => "se.omegapoint:cicd-lab-frontend:${version}",
+    repository => "public",
+    packaging  => 'tar.gz',
+    output     => "/var/tmp/cicd-lab-frontend-${version}.tar.gz",
+    ensure     => "update"
+  } ->
+  exec { "untar":
+    command => "/bin/tar xzvf /var/tmp/cicd-lab-frontend-${version}.tar.gz -C /var/www/html/"
+  }
+
+}
+
 node 'default' {
 			      
-    $backend_version = hiera("cicd-lab-backend_version")
+  $backend_version = hiera("cicd-lab-backend_version")
+  $frontend_version = hiera("cicd-lab-frontend_version")
 
-    backend_app { 'backend_app':
+  backend_app { 'backend_app':
     version => $backend_version
-    }
+  } ->
+  frontend_app { 'frontend_app':
+    version => $frontend_version
+  }
+
 }
